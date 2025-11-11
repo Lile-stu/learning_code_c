adc0fa3654fe7105043b57d68391c6a8bc1ee39a
Millenlun <lile.stu@qq.com>
2025-11-11 20:07:26 +0800

    整合

--- Wengkai_video_training(done)/TEXT.C
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(void)
-{
-    printf("hello world!");
-    printf("你好世界!");
-    return 0;
-}
--- Wengkai_video_training(done)/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> 
-
-int main()
-{ 
-	printf("%d",12+34);
-	
-	return 0;
-}
-
